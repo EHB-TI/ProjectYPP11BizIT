89ae3ecc091ecaaa5591d5d94f2a0cae9d1620fb
Julie Toussaint <julie.toussaint@student.ehb.be>
2020-05-01 10:06:34 +0000

    Update 1/05

--- webapp/controller/Homepage.controller.js
+++ webapp/controller/Homepage.controller.js
@@ -33,11 +33,10 @@ sap.ui.define([
 			reader.onload = function (e) {
 				var bin = e.target.result; // haal resultaat op van file upload
 				var array = bin.split("\t"); // split files in een array van objecten
-				for (var z = 0; z < array.length; z++) {
-					if (array[z] === "" || array[z] === "\t") {
-						array.splice(z, 1);
-					}
-				}
+				// 		for (var z = 0; z < array.length; z++) {
+				// 		array[z].split(/\n/g).join("");
+				// 	}
+
 				console.log("Array: " + array);
 
 				var aLines = [];
@@ -48,61 +47,73 @@ sap.ui.define([
 
 				if (name === "SCHEDULE_LINE_DATA.txt") {
 					console.log("Schedule line data detected!");
-					titleArray = ["Batch Input Interface Record Type", "Material Number", "Requirements type",
-						"Version number for independent requirements", "Indicator: version active", "Requirements Plan Number", "Plant",
-						"Name of info structure - characteristic values", "Field name in the generated DDIC structure",
-						"Version number in the information structure", "Account Assignment Category", "Special Stock Indicator",
-						"Consumption Posting", "Work Breakdown Structure Element (WBS Element)", "Item Number in Sales Order", "Sales Order Number",
-						"Reference type	Date", "Time", "Valuation of Special Stock", "MRP Area", "With no MRP"
+					titleArray = ["Material Number", "Batch Input Interface Record Type", "Period indicator (day, week, month, posting period)",
+						"Schedule line date", "Planned quantity batch input", "BOM explosion number", "Production Version",
+						"Offset for generation of test data"
 					];
 					for (var i1 = 0; i1 < titleArray.length; i1++) {
 						headerTitles[i1].innerHTML = titleArray[i1];
 						// headerBox[i1].setAttribute("style", "width: 100%!important");
 					}
-					for (var i2 = 0; i2 < array.length; i2++) {
-						var oLineScheduleLine1 = {
-							header1: array[i2]
-						};
-						aLines.push(oLineScheduleLine1);
+					var oLineScheduleLine1 = {
+						header1: array[0],
+						header2: array[1],
+						header3: array[2],
+						header4: array[3],
+						header5: array[4],
+						header6: "",
+						header7: "",
+						header8: "0"
+					};
+					for (var headera in oLineScheduleLine1) {
+						console.log("Before: " + oLineScheduleLine1[headera]);
+						oLineScheduleLine1[headera].split("/\n/g").join("");
+						console.log("After: " + oLineScheduleLine1[headera]);
 					}
+					console.log(oLineScheduleLine1);
+					aLines.push(oLineScheduleLine1);
+
+					for (var vakje = 7; vakje < array.length; vakje++) {
+						var oLineScheduleLine2 = {
+							header1: array[vakje],
+							header2: array[vakje + 1],
+							header3: array[vakje + 2],
+							header4: array[vakje + 3],
+							header5: array[vakje + 4],
+							header6: "",
+							header7: "",
+							header8: array[vakje + 7]
+						};
+						for (var headerb in oLineScheduleLine1) {
+							console.log("Before: " + oLineScheduleLine1[headerb]);
+							oLineScheduleLine1[headerb].split("/\n/g").join("");
+							console.log("After: " + oLineScheduleLine1[headerb]);
+						}
+						console.log(oLineScheduleLine2);
+						aLines.push(oLineScheduleLine2);
+					}
+
 					that.getView().getModel("FileModel").setData(aLines);
 					that.onLoadFileModel(aLines);
 				} else if (name === "ITEM_DATA.txt") {
 					console.log("Item data detected!");
-					titleArray = ["Material Number", "Batch Input Interface Record Type", "Period indicator (day, week, month, posting period)",
-						"Schedule line date", "Planned quantity batch input", "BOM explosion number", "Production Version",
-						"Offset for generation of test data"
+					titleArray = ["Batch Input Interface Record Type", "Material Number", "Requirements type",
+						"Version number for independent requirements", "Indicator: version active", "Requirements Plan Number", "Plant",
+						"Name of info structure - characteristic ", "Field name in the generated DDIC structure",
+						"Version number in the information structure", "Account Assignment Category", "Special Stock Indicator",
+						"Consumption Posting", "Work Breakdown Structure Element (WBS Element)", "Item Number in Sales Order", "Sales Order Number",
+						"Reference type	Date", "Time", "Valuation of Special Stock", "MRP Area", "With no MRP"
 					];
-					for (var j1 = 0; j1 < titleArray.length; j1++) {
-						headerTitles[j1].innerHTML = titleArray[j1];
-						// headerBox[j1].setAttribute("style", "width: 100%!important");
-					}
-					for (var j2 = 0; j2 < array.length; j2++) {
-						var oLineItem = {
-							header1: array[j2]
-						};
-						aLines.push(oLineItem);
-					}
 					that.getView().getModel("FileModel").setData(aLines);
 					that.onLoadFileModel(aLines);
-				} else if (name === "SESSION_RECORD.txt") {
+				} else
+				if (name === "SESSION_RECORD.txt") {
 					console.log("Session record data detected!");
 					titleArray = ["Material Number", "Batch Input Interface Record Type", "Delivery/order finish date", "Internal Class Number",
 						"Row Number of Variant Table - External", "Usage Probability in Character Format", "Fixing indicator",
 						"Copying firmed objects allowed", "Indicator = 'X' quantity / indicator = ' ' usage probability"
 					];
-					for (var x1 = 0; x1 < titleArray.length; x1++) {
-						headerTitles[x1].innerHTML = titleArray[x1];
-						// headerBox[x1].setAttribute("style", "width: 100%!important");
-					}
-					for (var x2 = 0; x2 < array.length; x2++) {
-						var oLineSeshRec = {
-							header1: array[x2]
-						};
-						aLines.push(oLineSeshRec);
-					}
 					that.getView().getModel("FileModel").setData(aLines);
-					//	oLine.setData(array);
 					that.onLoadFileModel(aLines);
 				}
 			};
