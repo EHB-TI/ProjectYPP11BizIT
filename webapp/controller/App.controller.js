f89a78267197bd889ea778f69e5ccab2383c0749
Joyce Hanssens <hanssens-j@hotmail.com>
2020-06-04 14:32:10 +0000

    Cedric test

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,4 +1,5 @@
 sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
+	
     "use strict";
     return Controller.extend("ProjectYPP11BizIT.ProjectYPP11BizIT.controller.App", {
         onInit: function () {
